3d7f5ef56711077444c2128cb70a76a0aa440c09
Sean Lawrence <lawrensm@gmail.com>
2016-09-19 00:57:47 -0400

    add menu with play and pause, seek, and fullscreen

--- src/menu.js
+++ src/menu.js
@@ -6,16 +6,41 @@
 
     var template = "\<div>\
             <div class='simple-360-controls'>\
-                <div class='simple-360-control simple-360-play menuicon menuicon-play'></div>\
+                <div role='button' class='simple-360-control simple-360-play menuicon menuicon-play'></div>\
                 <div class='simple-360-control simple-360-time'></div>\
-                <div class='simple-360-volume-container'>\
-                    <div class='simple-360-control simple-360-volume menuicon menuicon-volume'></div>\
-                    <div class='simple-360-volume-slider'></div>\
+                <div role='button' class='simple-360-control simple-360-full menuicon menuicon-maximize'></div>\
+                <div class='simple-360-progress'>\
+                    <input type='range' value='0' class='simple-360-progress-input'/>\
                 </div>\
-                <div class='simple-360-control simple-360-full menuicon menuicon-maximize'></div>\
-                <div class='simple-360-progress'></div>\
             </div>\
-        </div>";
+        </div>",
+
+        formatTime = function (secs) {
+            //NaN check
+            if (secs !== secs) {
+                return '0:00';
+            }
+            var h = Math.floor(secs / 3600);
+            if (h) {
+                secs = secs % 3600;
+            }
+            var m = Math.floor(secs / 60);
+            var s = Math.floor(secs - (m * 60)) < 10 ? '0' + Math.floor(secs - (m * 60)) : Math.floor(secs - (m * 60));
+            if (h) {
+                return h + ':' + (m < 10 ? '0' + m : m) + ':' + s;
+            }
+            return m + ':' + s;
+        },
+
+        getDims = function () {
+            var w = window,
+                d = document,
+                e = d.documentElement,
+                g = d.getElementsByTagName('body')[0],
+                x = w.innerWidth || e.clientWidth || g.clientWidth,
+                y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+            return { w: x, h: y };
+        };
 
     function htmlToElements(html) {
         var template = document.createElement('template');
@@ -24,10 +49,105 @@
     }
 
     function Menu(player) {
+        var _this = this;
+
         this.container = player.container;
         this.container.appendChild(htmlToElements(template)[0]);
+        this.container.className += " minscreen";
+        this.video = player.el;
+        this.canvas = player.canvas;
 
+        this.initializedCanvasWidth = this.canvas.width;
+        this.initializedCanvasHeight = this.canvas.height;
+
+        this._playPause = document.getElementsByClassName('simple-360-play')[0];
+        this._progressSlider = document.getElementsByClassName('simple-360-progress-input')[0];
+        this._minMaxScreen = document.getElementsByClassName('simple-360-full')[0];
+        this._time = document.getElementsByClassName('simple-360-time')[0];
+
+
+        /**
+         * Add event listeners
+         */
+
+        this._playPause.addEventListener("click", function () {
+            if (_this.video.paused) {
+                this.className = this.className.replace("menuicon-play", "menuicon-pause");
+                _this.video.play();
+            } else {
+                this.className = this.className.replace("menuicon-pause", "menuicon-play");
+                _this.video.pause();
+            }
+        });
+
+        this.isFullscreen = false;
+        this._minMaxScreen.addEventListener("click", function() {
+            if (!_this.isFullscreen) {
+                if (_this.canvas.requestFullscreen) {
+                    _this.canvas.requestFullscreen();
+                } else if (_this.canvas.mozRequestFullScreen) {
+                    _this.canvas.mozRequestFullScreen(); // Firefox
+                } else if (_this.canvas.webkitRequestFullscreen) {
+                    _this.canvas.webkitRequestFullscreen(); // Chrome and Safari
+                }
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                }
+            }
+        });
+
+        function resize() {
+            var dims = getDims(),
+                width = _this.isFullscreen ? dims.w : _this.initializedCanvasWidth,
+                height = _this.isFullscreen ? dims.h : _this.initializedCanvasHeight;
+
+            _this.canvas.width = width;
+            _this.canvas.height = height;
+        }
+
+        function onFschange() {
+            var fullscreenElement = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
+            console.log('here');
+            if (!fullscreenElement) {
+                _this.isFullscreen = false;
+                _this.container.className = _this.container.className.replace("fullscreen", "minscreen");
+            } else {
+                _this.isFullscreen = true;
+                _this.container.className = _this.container.className.replace("minscreen", "fullscreen");
+            }
+            resize();
+
+        }
+        document.addEventListener('mozfullscreenchange', onFschange);
+        document.addEventListener('webkitfullscreenchange', onFschange);
+        document.addEventListener('fullscreenchange', onFschange);
+
+        this._progressSlider.addEventListener("change", function() {
+            var newTime = _this.video.duration * (_this._progressSlider.value / 100);
+            _this.video.currentTime = newTime;
+        });
+
+        this.video.addEventListener("timeupdate", function () {
+            var percent = _this.video.currentTime / _this.video.duration;
+            _this._progressSlider.value = percent * 100;
+            _this.updateTime();
+        }, false);
+
+        this.video.addEventListener("loadedmetadata", function () {
+            _this.updateTime();
+        }, false);
     }
+
+    Menu.prototype.updateTime = function(time) {
+        var duration = this.video.duration || 0,
+            time = time || this.video.currentTime || 0;
+        this._time.textContent = formatTime(time) + ' / ' + formatTime(duration);
+    };
 
     Menu.prototype.update = function(player, delta) {
 
